f47b91c8a17e2e8cc8ba7e6edc012d95d52ba8b9
Sergio Castaño Arteaga <tegioz@icloud.com>
2024-03-04 14:09:20 +0100

    Upgrade deps, base images and GH actions (#469)
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- src/github.rs
+++ src/github.rs
@@ -1,11 +1,10 @@
 use crate::cfg::CfgProfile;
 use anyhow::{Error, Result};
 use async_trait::async_trait;
-use axum::http::HeaderValue;
+use axum::http::{HeaderValue, StatusCode};
 #[cfg(test)]
 use mockall::automock;
 use octocrab::{models::InstallationId, Octocrab, Page};
-use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use std::sync::Arc;
